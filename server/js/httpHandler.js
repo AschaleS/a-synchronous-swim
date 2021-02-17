474850ec7dd41c39b25e01efc934ffabe6ec9da5
Christopher Raffaele <alumnye@gmail.com>
2021-02-16 23:04:36 -0500

    Complete implementing swim command

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -13,8 +13,26 @@ module.exports.initialize = (queue) => {
 };
 
 module.exports.router = (req, res, next = ()=>{}) => {
+
+  if(req.method === 'GET') {
+    console.log('ping');
+  }
+
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
+  let movement = getRandom();
+  res.write(movement);
+  // req.method ---> GET, POST, etc (type of request)
+  // if res.method is GET, do something
+  // res.write ---> body of the response
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
+};
+
+
+// helper function
+let getRandom = function() {
+  let commands = ['left', 'right', 'up', 'down'];
+  let r = Math.floor(Math.random() * commands.length)
+  return commands[r];
 };
